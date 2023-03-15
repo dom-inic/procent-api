9b15f5f7137945fa81ea80ffb63992f8ca00ee40
dom-inic <dominicnyambane22@gmail.com>
2023-03-16 00:41:08 +0300

    rpoducts serializers

---
+++ products/serializers.py
@@ -0,0 +1,17 @@
+from dataclasses import fields
+from rest_framework import serializers
+from . models import Product,Category,ProductImage
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+class ProductImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
