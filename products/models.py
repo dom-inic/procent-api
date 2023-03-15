83b027d5d65311b2df171910ad8b3b502db85095
dom-inic <dominicnyambane22@gmail.com>
2023-03-15 23:01:53 +0300

    products app, models

---
+++ products/models.py
@@ -0,0 +1,38 @@
+from tabnanny import verbose
+from unicodedata import category
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self) -> str:
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    brand = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    stock = models.IntegerField()
+    weight = models.DecimalField(max_digits=10, decimal_places=2)
+    dimensions = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ['-id'] #order by id in descending order
+
+    def __str__(self) -> str:
+        return self.name
+    
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product_images/')
+
+    def __str__(self) -> str:
+        return self.image.url
