6f16981eccf53794d866cb42d7e7066fe5af57a5
dom-inic <dominicnyambane22@gmail.com>
2023-07-29 18:31:04 +0300

    registering models

--- shop/models.py
+++ shop/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=200, db_index=True)
-    sulg = models.SlugField(max_length=200,unique=True)
+    slug = models.SlugField(max_length=200,unique=True)
 
     class Meta:
         ordering = ('name',)
