6ee9dd6bb981814087f5fc66d96b90ce6f8e8730
dom-inic <dominicnyambane22@gmail.com>
2023-08-04 23:12:25 +0300

    order detail admin

--- orders/admin.py
+++ orders/admin.py
@@ -3,6 +3,8 @@ from . models import Order, OrderItem
 import csv 
 import datetime
 from django.http import HttpResponse
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 def export_to_csv(modeladmin, request, queryset):
     """ admin action to export orders to csv """
@@ -33,10 +35,14 @@ class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product',]
 
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}>View</a>')
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email', 'address', 
-                    'postal_code', 'city', 'paid', 'created', 'updated'
+                    'postal_code', 'city', 'paid', 'created', 'updated',
                     ]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
