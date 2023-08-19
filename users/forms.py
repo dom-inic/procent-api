18d0bb8b2b95379e65d2f5a75e5ac785c36fec29
dom-inic <dominicnyambane22@gmail.com>
2023-08-19 15:17:25 +0300

    register

--- users/forms.py
+++ users/forms.py
@@ -1,3 +1,16 @@
 from django import forms 
 from django.contrib.auth.models import User
 
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='password', widget=forms.PasswordInput)
+    password = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name','email')
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError('passwords don\'t match')
+        return cd['password2']
--- users/views.py
+++ users/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from . forms import UserRegistrationForm
 
 @login_required
 def dashbaord(request):
-    return render(request, 'users/register.html', {'section': 'dashboard'})
+    return render(request, 'users/dashboard.html', {'section': 'dashboard'})
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # set the chosen password 
+            new_user.set_password(
+                user_form.cleaned_data['password']
+            )
+            # save user object
+            new_user.save()
+            return render(request, 'users/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'users/register.html', {'user_form':user_form})
