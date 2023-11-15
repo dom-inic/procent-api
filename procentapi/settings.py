98f79eb4761928c8d814cb561f4bc18b3746cb03
dom-inic <dominicnyambane22@gmail.com>
2023-11-15 21:28:28 +0300

    docker, postgres setup

--- procentapi/settings.py
+++ procentapi/settings.py
@@ -101,6 +101,18 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': 'db',
+#         'PORT': 5432
+#     }
+# }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
