5a4e6f8fe8d1df1a9bd80e2a3d3f672a138deb7b
dom-inic <dominicnyambane22@gmail.com>
2023-11-27 08:40:06 +0300

    fix

--- procentapi/settings.py
+++ procentapi/settings.py
@@ -202,10 +202,10 @@ BRAINTREE_CONF = braintree.Configuration(
     BRAINTREE_PRIVATE_KEY
 )
 
-REDIS_HOST = 'ec2-52-70-18-167.compute-1.amazonaws.com'
-REDIS_PORT = '23940'
-REDIS_DB = 1
+# REDIS_HOST = 'ec2-52-70-18-167.compute-1.amazonaws.com'
+# REDIS_PORT = '23940'
+# REDIS_DB = 1
 
-# # Celery Configuration docker
-CELERY_BROKER_URL = 'amqps://mnblvtxv:Sn6DfuO620YOAZ3eikewGOn4mycnV1yz@fish.rmq.cloudamqp.com/mnblvtxv'
-CELERY_RESULT_BACKEND = 'redis://ec2-52-70-18-167.compute-1.amazonaws.com:23940/1'
+# # # Celery Configuration docker
+# CELERY_BROKER_URL = 'amqps://mnblvtxv:Sn6DfuO620YOAZ3eikewGOn4mycnV1yz@fish.rmq.cloudamqp.com/mnblvtxv'
+# CELERY_RESULT_BACKEND = 'redis://ec2-52-70-18-167.compute-1.amazonaws.com:23940/1'
