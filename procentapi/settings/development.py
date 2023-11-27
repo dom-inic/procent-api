925dfa667f30e4f6a38bd932ee8129d239230d94
dom-inic <dominicnyambane22@gmail.com>
2023-11-27 13:24:43 +0300

    redis config

--- procentapi/settings/development.py
+++ procentapi/settings/development.py
@@ -21,4 +21,4 @@ REDIS_PORT = '16340'
 REDIS_DB = 1
 
 # Celery Configuration docker
-CELERY_BROKER_URL = 'rediss://:p6da119e6d04a68daf6bfa00b216a2a45605feceee98f003355d56c3c9d177b0d@ec2-44-196-160-1.compute-1.amazonaws.com:16340/0'
+CELERY_BROKER_URL = 'rediss://:p6da119e6d04a68daf6bfa00b216a2a45605feceee98f003355d56c3c9d177b0d@ec2-44-196-160-1.compute-1.amazonaws.com:16340'
