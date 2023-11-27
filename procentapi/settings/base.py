d067c321614acaf883c4d93132a734145655323b
dom-inic <dominicnyambane22@gmail.com>
2023-11-27 12:16:58 +0300

    secretkey

--- procentapi/settings/base.py
+++ procentapi/settings/base.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-(i=u9mys*bafg*p!_-lzck8_rs^!zr4@+h0b$4rr9589t8e7i^'
 
 
 # Application definition
