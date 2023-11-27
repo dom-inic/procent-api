1996a474a81cc347b01726ddf5cc3ae9fa1632bd
dom-inic <dominicnyambane22@gmail.com>
2023-11-27 14:29:57 +0300

    heroku postgres

--- procentapi/settings/development.py
+++ procentapi/settings/development.py
@@ -11,8 +11,13 @@ ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'peaceful-badlands-38433-2cbf049590ab.h
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASS'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
+        
     }
 }
 
