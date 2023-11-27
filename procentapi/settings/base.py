cb19e14ff0199e0124d1a2ade8dd801512a1dd15
dom-inic <dominicnyambane22@gmail.com>
2023-11-27 22:40:13 +0300

    media storage on s3 buckets

--- procentapi/settings/base.py
+++ procentapi/settings/base.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rosetta',
     'parler',
     'localflavor',
+    'storages'
 
     # apps
     'shop.apps.ShopConfig',
@@ -55,6 +56,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -62,7 +64,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'procentapi.urls'
--- procentapi/settings/development.py
+++ procentapi/settings/development.py
@@ -21,6 +21,21 @@ DATABASES = {
     }
 }
 
+AWS_ACCESS_KEY_ID=os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY=os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME=os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_QUERYSTRING_AUTH = False
+
+# s3 storages
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_HEADERS = {  
+    # see http://developer.yahoo.com/performance/rules.html#expires
+    'Expires': 'Sun, 27 Feb 2099 20:00:00 GMT',
+    'Cache-Control': 'max-age=94608000',
+}
 REDIS_HOST = 'ec2-44-196-160-1.compute-1.amazonaws.com'
 REDIS_PORT = '16340'
 REDIS_DB = 1
